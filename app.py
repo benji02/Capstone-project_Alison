8ae5e4ea570a38f10530f9032882ee612e0684b3
Benjamin Lavoie <benji0202@gmail.com>
2024-04-02 19:45:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -75,10 +75,10 @@ predict_button = st.button('Predict', key='predict_button')
 # Make prediction when the 'Predict' button is clicked
 if predict_button:
 
-    print(HG38_Start_df)
+    # print(HG38_Start_df)
     print(int(HG38_Start))
     print(type(HG38_Start)) 
-    print(type(HG38_Start_df))
+    # print(type(HG38_Start_df))
 
     # Convert inputs to integers
     HG38_Start_df = pd.DataFrame([int(HG38_Start)], columns='HG38_Start')
